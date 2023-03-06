395b4b77db87a9e4a6798befa444bcc2cb6150d6
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-03-06 16:31:23 +0000

    updated index, style.css, and script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 Shyla Oommen All rights reserved
+//
+// Created by: Shyla Oommen
+// Created on: Mar 2023
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello, World!"
+  
+document.getElementById("hello-world").innerHTML = "<p>I coded this in JavaScript!</p>"
+}
